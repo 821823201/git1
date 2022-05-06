390813114fa1dace1845eb02a7474e3467e597ac
Your Name <you@example.com>
2022-05-06 10:14:06 +0800

    hot_fix second commit

--- src/com/github.java
+++ src/com/github.java
@@ -4,5 +4,6 @@ public class github {
     public static void main(String[] args) {
         System.out.println("hello!git");
         System.out.println("hello!idea111");
+        System.out.println("hello!idea111");
     }
 }
